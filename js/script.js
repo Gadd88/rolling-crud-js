fec8965be34db79be99e1d85f3748d7ac7063773
Matias Saade <matiassaade@gmail.com>
2023-12-20 11:49:42 -0300

    funcion de crear producto

--- js/script.js
+++ js/script.js
@@ -1,49 +1,63 @@
-// {
-//     producto:,
-//     descripcion:,
-//     precio:,
-//     stock:,
-// }
-
-// const productos = [
-//     {
-//         producto:1,
-//         descripcion:'aaa',
-//         precio: 200,
-//         stock: 10,
-//     },
-//     {
-//         producto: 2,
-//         descripcion: 'bbbb',
-//         precio: 500,
-//         stock: 5,
-//     }
-// ]
 
 const nombreProducto = document.getElementById('inputProducto')
+const categoriaProducto = document.getElementById('inputCategoria')
 const descripcionProducto = document.getElementById('inputDescripcion')
 const precioProducto = document.getElementById('inputPrecio')
 const stockProducto = document.getElementById('inputStock')
 const btnAgregarProducto = document.getElementById('btn-agregar')
+const listaProductos = document.getElementById('lista-productos')
 let productos = []
 
+//Agregar Producto
 const agregarProducto = () => {
     const producto = {
         id: crypto.randomUUID().slice(0,4),
-        producto: nombreProducto.value,
+        nombre: nombreProducto.value,
+        categoria: categoriaProducto.value,
         descripcion: descripcionProducto.value,
         precio: precioProducto.value,
         stock: stockProducto.value,
     }
     productos.push(producto)
-    console.log(productos)
     nombreProducto.value = ''
+    categoriaProducto.value = ''
     descripcionProducto.value = ''
     precioProducto.value = ''
     stockProducto.value = ''
 }
-
-btnAgregarProducto.addEventListener('click', () => {
+btnAgregarProducto.addEventListener('click', (e) => {
     e.preventDefault()
     agregarProducto()
+    mostrarProductos()
 })
+
+//Mostrar productos
+
+const mostrarProductos = () => {
+    const listaProductos = document.getElementById('lista-productos')
+    listaProductos.innerHTML = ''
+    productos.forEach((producto) => {
+
+        const {id, categoria, nombre, descripcion, stock, precio} = producto
+
+        listaProductos.innerHTML += `
+        <li class="list-group-item d-flex justify-content-between align-items-start col-4" id=${id}>
+            
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <h5 class="card-title">${nombre}</h5>
+                        <button type='button' class="bg-danger text-white borrar rounded-pill col-2" id="btn-eliminar">X</button>
+                    </div>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">${categoria}</h6>
+                    <p class="card-text">${descripcion}</p>
+                    <div class="d-flex justify-content-between">
+                        <span class="p-2 col-5">${stock}UN.</span>
+                        <span class="bg-success text-white fw-bold rounded-none p-2 col-5">$${precio}</span>
+                    </div>
+                </div>
+            </div>
+        </li>
+        `
+    })
+}
